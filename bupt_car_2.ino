092a2681d40eaa56bb24f76ff3d7aadf1213ddb1
dannyHallo <ruitian0716@gmail.com>
2022-09-22 23:52:33 +0800

    better ccd

--- bupt_car_2.ino
+++ bupt_car_2.ino
@@ -1,96 +1,94 @@
-#include "dep/pinouts.h"
+#include "dep/bluetooth.h"
 #include "dep/boardLed.h"
 #include "dep/ccd.h"
-#include "dep/servo.h"
-#include "dep/motor.h"
-#include "dep/bluetooth.h"
 #include "dep/commandParser.h"
-#include "dep/naviLine.h"
+#include "dep/motor.h"
+// #include "dep/naviLine.h"
 #include "dep/pid.h"
+#include "dep/pinouts.h"
+#include "dep/servo.h"
 
 TaskHandle_t Task1Handle;
 TaskHandle_t Task2Handle;
 
 int trackMidPoint = -1;
-int command = -1;
-naviLine navi = naviLine();
+int command       = -1;
+// naviLine navi     = naviLine();
 
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
 
-    pinoutInitBoardLed();
-    pinoutInitCCD();
-    pinoutAndPwmChannelInitServo();
-    pinoutAndPwmChannelInitMotor();
-    pinoutInitAndOpenBTSerialBluetooth();
+  pinoutInitBoardLed();
+  pinoutInitCCD();
+  pinoutAndPwmChannelInitServo();
+  pinoutAndPwmChannelInitMotor();
+  pinoutInitAndOpenBTSerialBluetooth();
 
-    assignTasks();
+  assignTasks();
 
-    navi.initNaviLine();
+//   navi.initNaviLine();
 
-    Serial.printf("Clocck cycle: %lld\n",clockCycle);
+  Serial.printf("Clocck cycle: %lld\n", clockCycle);
 }
 
 void assignTasks() {
-    xTaskCreatePinnedToCore(
-        Task1,        // Task function
-        "Task1",      // Task name
-        2000,         // Stack size
-        NULL,         // Parameter
-        1,            // Priority
-        &Task1Handle, // Task handle to keep track of created task
-        0             // Core ID: 0:
-    );
+  xTaskCreatePinnedToCore(Task1,        // Task function
+                          "Task1",      // Task name
+                          2000,         // Stack size
+                          NULL,         // Parameter
+                          1,            // Priority
+                          &Task1Handle, // Task handle to keep track of created task
+                          0             // Core ID: 0:
+  );
 
-    xTaskCreatePinnedToCore(
-        Task2,        // Task function
-        "Task2",      // Task name
-        2000,         // Stack size
-        NULL,         // Parameter
-        1,            // Priority
-        &Task2Handle, // Task handle to keep track of created task
-        1             // Core ID: 0:
-    );
+  xTaskCreatePinnedToCore(Task2,        // Task function
+                          "Task2",      // Task name
+                          2000,         // Stack size
+                          NULL,         // Parameter
+                          1,            // Priority
+                          &Task2Handle, // Task handle to keep track of created task
+                          1             // Core ID: 0:
+  );
 }
 
 // This loop is automatically assigned to Core 1, so block it manually
 void loop() {
-    //Serial.printf("Clocck cycle: %lld",clockCycle);
-    delay(1000);
-    // navi.printBinarizedPixels();
-    // navi.printLTBrightness();
-    // navi.printLinearPixels();
+  // Serial.printf("Clocck cycle: %lld",clockCycle);
+  delay(1000);
+  // navi.printBinarizedPixels();
+  // navi.printLTBrightness();
+  // navi.printLinearPixels();
 }
 
 void Task1(void* pvParameters) {
-    for (;;) {
-        if (Serial.available()) {
-            btSend(Serial.read());
-        }
-
-        command = btRecieve();
-        vTaskDelay(20);
+  for (;;) {
+    if (Serial.available()) {
+      btSend(Serial.read());
     }
+
+    command = btRecieve();
+    vTaskDelay(20);
+  }
 }
 
 void Task2(void* pvParameters) {
-    for (;;) {
-        //Serial.printf("Clocck cycle: %lld\n",clockCycle);
-        // motorLoop();
+  for (;;) {
+    // Serial.printf("Clocck cycle: %lld\n",clockCycle);
+    //  motorLoop();
 
-        trackMidPoint = processCCD();
-        // Serial.println(trackMidPoint);
+    trackMidPoint = processCCD();
+    // Serial.println(trackMidPoint);
 
-        if (trackMidPoint!=-1) {
-            //servoLoop(getPID(trackMidPoint));
-            servoLoop(trackMidPoint);
-        }
-
-        // int direction = navi.getMidLine();
-
-        // servoWriteAngle(scaleAngle(direction,0,45,1000));
-        // Serial.printf("Direction: %d\n",direction);
-
-        // vTaskDelay(5);
+    if (trackMidPoint != -1) {
+      // servoLoop(getPID(trackMidPoint));
+      servoLoop(trackMidPoint);
     }
+
+    // int direction = navi.getMidLine();
+
+    // servoWriteAngle(scaleAngle(direction,0,45,1000));
+    // Serial.printf("Direction: %d\n",direction);
+
+    // vTaskDelay(5);
+  }
 }
--- dep/bluetooth.h
+++ dep/bluetooth.h
@@ -1,29 +1,25 @@
 #pragma once
 
+#include "boardLed.h"
 #include "pinouts.h"
+
 #include "../lib/arduino-esp32/libraries/BluetoothSerial/src/BluetoothSerial.h"
 
 BluetoothSerial serialBT;
 
-void pinoutInitAndOpenBTSerialBluetooth()
-{
-    serialBT.begin("ESP32Test");
-    Serial.println("Bluetooth configured, now you can pair it!");
+void pinoutInitAndOpenBTSerialBluetooth() {
+  serialBT.begin("ESP32Test");
+  Serial.println("Bluetooth configured, now you can pair it!");
 }
 
-void btSend(int message)
-{
-    serialBT.write(message);
-}
+void btSend(int message) { serialBT.write(message); }
 
-int btRecieve()
-{
-    if (serialBT.available())
-    {
-        int message = serialBT.read();
+int btRecieve() {
+  if (serialBT.available()) {
+    int message = serialBT.read();
 
-        flipBoardLed();
-        return message;
-    }
-    return -1;
+    flipBoardLed();
+    return message;
+  }
+  return -1;
 }
