0423681ade8eddae97954df70dc5a4703387c71d
dannyHallo <ruitian0716@gmail.com>
2022-10-07 18:39:13 +0800

    simpler ccd

--- bupt_car_2.ino
+++ bupt_car_2.ino
@@ -1,5 +1,6 @@
 // #define BT_ON
 
+#include "args.h"
 #include "dep/bluetooth.h"
 #include "dep/boardLed.h"
 #include "dep/ccd.h"
@@ -10,7 +11,6 @@
 #include "dep/pid.h"
 #include "dep/pinouts.h"
 #include "dep/servo.h"
-#include "args.h"
 
 TaskHandle_t Task1Handle;
 TaskHandle_t Task2Handle;
@@ -19,183 +19,208 @@ int command = -1;
 
 int location = 0;
 
-pid angelPID(angle_kp,angle_ki,angle_kd);
+pid angelPID(angle_kp, angle_ki, angle_kd);
 
 void setup() {
-    Serial.begin(serial_btr);
+  Serial.begin(serial_btr);
 
-    pinoutInitBoardLed();
-    initColor();
-    initCCD();
-    initServo();
-    initMotor();
-    initBluetooth();
-    initOled();
+  pinoutInitBoardLed();
+  initColor();
+  initCCD();
+  initServo();
+  initMotor();
+  initBluetooth();
+  initOled();
+  pinMode(PINOUT_MOTOR_ON, INPUT_PULLDOWN);
 
-    assignTasks();
+  display.clearDisplay();
+  oledPrint("Booting...", 0);
+  oledFlush();
+  Serial.println("Booting...");
 
-    pinMode(PINOUT_MOTOR_ON,INPUT_PULLDOWN);
+  assignTasks();
 }
 
 void assignTasks() {
-    xTaskCreatePinnedToCore(Task1,        // Task function
-        "Task1",      // Task name
-        2000,         // Stack size
-        NULL,         // Parameter
-        1,            // Priority
-        &Task1Handle, // Task handle to keep track of created task
-        0             // Core ID: 0:
-    );
+  xTaskCreatePinnedToCore(Task1,        // Task function
+                          "Task1",      // Task name
+                          65535,        // Stack size
+                          NULL,         // Parameter
+                          1,            // Priority
+                          &Task1Handle, // Task handle to keep track of created task
+                          0             // Core ID: 0:
+  );
 
-    xTaskCreatePinnedToCore(Task2,        // Task function
-        "Task2",      // Task name
-        2000,         // Stack size
-        NULL,         // Parameter
-        1,            // Priority
-        &Task2Handle, // Task handle to keep track of created task
-        1             // Core ID: 0:
-    );
+  xTaskCreatePinnedToCore(Task2,        // Task function
+                          "Task2",      // Task name
+                          65535,        // Stack size
+                          NULL,         // Parameter
+                          1,            // Priority
+                          &Task2Handle, // Task handle to keep track of created task
+                          1             // Core ID: 0:
+  );
 }
 
-void autoTrack(int bestExplosureTime,float minThrehold) {
-    bool motorEnable = digitalRead(PINOUT_MOTOR_ON) ? true : false;
+void autoTrack(int explosureTime, int originalExplosureTime, float threhold) {
+  bool motorEnable = digitalRead(PINOUT_MOTOR_ON) ? true : false;
 
-    int trackMidPixel = 0;
-    float usingThrehold = 0;
-    int trackStatus = 0;
+  int trackMidPixel = 0;
+  int trackStatus   = 0;
 
-    bool isPlatform = false;
+  processCCD(trackMidPixel, trackStatus, explosureTime, threhold, true);
 
-    processCCD(trackMidPixel,usingThrehold,trackStatus,bestExplosureTime,minThrehold,true);
+  // Print status only
+  switch (trackStatus) {
+  case STATUS_NORMAL:
+    boardLedOff();
+    oledPrint("tracking", 1);
+    break;
+  case STATUS_NO_TRACK:
+    boardLedOff();
+    oledPrint("notrack", 1);
+    break;
+  case STATUS_PLATFORM:
+    boardLedOn();
+    oledPrint("platform", 1);
+    break;
+  }
 
-    switch (trackStatus) {
-    case STATUS_NORMAL:
-        // Show status
-        boardLedOff();
+  switch (trackStatus) {
+  case STATUS_NORMAL: {
+    servoWritePixel(angelPID.update(trackMidPixel - 64) + 64);
 
-        // Get val
-        servoWritePixel(angelPID.update(trackMidPixel-64)+64);
-        break;
+    float motorAimSpeed = motorEnable ? aim_speed : 0;
+    motorForward(motorAimSpeed);
+    break;
+  }
+  case STATUS_PLATFORM: {
+    motorBrake();
+    delay(2000);
 
-    case STATUS_HIGH_DL:
-    case STATUS_PLATFORM:
-        boardLedOn();
-        motorIdle();
-        vTaskDelay(200);
-        printColorToRow();
-        oledPrint(++location,"Location",1);
-        vTaskDelay(2000);
-        if (motorEnable) {
-            motorForward(aim_speed/2);
-        } else {
-            motorIdle();
-        }
-        vTaskDelay(500);
+    // printColorToRow(0);
+    servoWritePixel(64);
+    float motorAimSpeed = motorEnable ? aim_speed : 0;
+    motorForward(motorAimSpeed);
 
-        break;
-    case STATUS_NO_TRACK:
-        boardLedOn();
+    while (trackStatus == STATUS_PLATFORM)
+      processCCD(trackMidPixel, trackStatus, originalExplosureTime, threhold, true);
 
-        if (trackStatus==STATUS_HIGH_DL)
-            oledPrint("!highratio",1);
-        if (trackStatus==STATUS_NO_TRACK)
-            oledPrint("!notrack",1);
+    break;
+  }
+  default:
+    motorIdle();
+    break;
+  }
 
-        servoWritePixel(127);
-        break;
-    }
+  //   case STATUS_PLATFORM:
+  //     boardLedOn();
+  //     motorIdle();
+  //     vTaskDelay(200);
+  //     printColorToRow();
+  //     oledPrint(++location, "Location", 1);
+  //     vTaskDelay(2000);
+  //     if (motorEnable) {
+  //       motorForward(aim_speed / 2);
+  //     } else {
+  //       motorIdle();
+  //     }
+  //     vTaskDelay(500);
 
-    if (motorEnable) {
-        motorForward(aim_speed);
-    } else {
-        motorIdle();
-    }
+  //     break;
+  //   case STATUS_NO_TRACK:
+  //     boardLedOn();
+
+  //     servoWritePixel(127);
+  //     break;
+  //   }
 }
 
 // this loop is intentionally left blank
 void loop() { delay(1000); }
 
 void Task1(void* pvParameters) {
-    for (;;) {
-        if (Serial.available()) {
-            btSend(Serial.read());
-        }
-
-        command = btRecieve();
-        vTaskDelay(20);
+  for (;;) {
+    if (Serial.available()) {
+      btSend(Serial.read());
     }
+    command = btRecieve();
+    vTaskDelay(20);
+  }
 }
 
+void prepareCCD(bool& cameraIsBlocked, bool& bestAvailable, explosureRecord& bestRecord) {
+  display.clearDisplay();
+  cameraIsBlocked = false;
+  bestAvailable   = false;
 
-void mainLoop1() {
-    display.clearDisplay();
+  getBestExplosureTime(bestRecord, cameraIsBlocked, bestAvailable, false);
 
-    bool cameraIsBlocked = false;
-    bool bestAvailable = false;
+  Serial.println("----------------------------------------");
+  if (cameraIsBlocked) {
+    Serial.println("bluetooth mode activated");
+  } else {
+    Serial.println("tracking mode activated");
+  }
 
-    explosureRecord bestRecord;
-    getBestExplosureTime(bestRecord,cameraIsBlocked,bestAvailable,true);
+  if (bestAvailable) {
+    Serial.print("best explosure time: ");
+    Serial.print(bestRecord.explosureTime);
+    Serial.print(" with minimum ratio: ");
+    Serial.println(bestRecord.threhold);
+  } else {
+    Serial.println("no available record");
+  }
 
-    Serial.println("----------------------------------------");
-    if (cameraIsBlocked) {
-        Serial.print("camera blocked, ");
-        Serial.println("bluetooth mode activated");
-    } else {
-        Serial.print("camera available, ");
-        Serial.println("tracking mode activated");
-    }
+  Serial.println("----------------------------------------");
 
-    if (bestAvailable) {
-        Serial.print("Best explosure time: ");
-        Serial.print(bestRecord.explosureTime);
-        Serial.print(" with minimum ratio: ");
-        Serial.println(bestRecord.threhold);
-    } else {
-        Serial.println("No available record");
-    }
-
-    Serial.println("----------------------------------------");
-
-    oledPrint(bestRecord.explosureTime,"expl",0);
-    oledPrint(bestRecord.threhold,"thre",1);
-    oledFlush();
-    delay(2500);
-    display.clearDisplay();
-
-    if (cameraIsBlocked) {
-        oledPrint("BT MODE",1);
-        oledFlush();
-
-        for (;;) {
-            parseCommands(command);
-        }
-    } else {
-        oledPrint("TRACK MODE",1);
-        oledFlush();
-        delay(1000);
-
-        for (;;) {
-            display.clearDisplay();
-
-            printColorToRow(0);
-            autoTrack(bestRecord.explosureTime,bestRecord.threhold);
-            oledFlush();
-        }
-    }
+  oledPrint(bestRecord.explosureTime, "expl", 0);
+  oledPrint(bestRecord.threhold, "thre", 1);
+  oledFlush();
+  delay(2000);
+  display.clearDisplay();
 }
 
-void colorLoop() { getColor(); }
+int loopTime = 0;
+int prevTime = 0;
+
+int getTime() {
+  loopTime       = millis();
+  int prevTimeMs = loopTime - prevTime;
+  prevTime       = loopTime;
+  return prevTimeMs;
+}
 
 void Task2(void* pvParameters) {
-    // Greeting from core 1
-    display.clearDisplay();
-    oledPrint("Hello!",0);
-    oledFlush();
+  for (;;) {
+    bool cameraIsBlocked, bestAvailable;
+    explosureRecord bestRecord;
+    prepareCCD(cameraIsBlocked, bestAvailable, bestRecord);
 
-    Serial.println("Hello!");
+    if (cameraIsBlocked) {
+      oledPrint("BT MODE", 1);
+      oledFlush();
 
-    for (;;) {
-        // colorLoop();
-        mainLoop1();
+      for (;;) {
+        parseCommands(command);
+      }
+    } else {
+      oledPrint("TRACK MODE", 1);
+      oledFlush();
+      delay(1000);
+
+      for (;;) {
+        display.clearDisplay();
+
+        int prevTimeMs = getTime();
+        oledPrint(prevTimeMs, "time");
+
+        int thisTimeExplosureTime = bestRecord.explosureTime - prevTimeMs;
+        if (thisTimeExplosureTime < 0)
+          thisTimeExplosureTime = 0;
+
+        autoTrack(thisTimeExplosureTime, bestRecord.explosureTime, bestRecord.threhold + 0.2f);
+        oledFlush();
+      }
     }
+  }
 }
--- dep/ccd.h
+++ dep/ccd.h
@@ -5,30 +5,31 @@
 #include "oled.h"
 #include "pinouts.h"
 
-const int STATUS_NORMAL = 0;
-const int STATUS_HIGH_DL = 1;
-const int STATUS_NO_TRACK = 2;
-const int STATUS_PLATFORM = 3;
+const int STATUS_NORMAL   = 0;
+const int STATUS_NO_TRACK = 1;
+const int STATUS_PLATFORM = 2;
 
 // Hardware related
-const int cNumPixels = 128;
+const int cNumPixels  = 128;
 const int cCountStart = 15;
-const int cCountEnd = 126;
+const int cCountEnd   = 126;
 
 // Line detection
 const int cEffectiveLineWidthMin = 10;
 
 // Explosure time
 const int cDefaultExplosureTime = 10;
-const int cExplosureTimeStart = 10;
-const int cExplosureTimeEnd = 80;
-const int cExplosureTimePropagation = 5;
+// const int cExplosureTimeStart       = 10;
+// const int cExplosureTimeEnd         = 80;
+const int cExplosureTimeStart       = 60;
+const int cExplosureTimeEnd         = 140;
+const int cExplosureTimePropagation = 10;
 
 // Dark / light dynamic propagation
 const float cThreholdSearchingPropagationInit = 0.01f;
-const float cThreholdSearchingPropagation = 0.1f;
-const float cDarkRatioAbnormalMin = 0.3f;
-const float cDarkRatioAbnormalMax = 0.8f;
+const float cThreholdSearchingPropagation     = 0.1f;
+const float cDarkRatioAbnormalMin             = 0.3f;
+const float cDarkRatioAbnormalMax             = 0.8f;
 
 // Blocking condition
 const float cMinMaxRatioDeltaBlocked = 0.6f;
@@ -43,61 +44,55 @@ bool binaryOnehotData[cNumPixels]{};
 int avgMarkingVal = 0;
 
 void initCCD() {
-  pinMode(PINOUT_CCD_SI,OUTPUT);
-  pinMode(PINOUT_CCD_CLK,OUTPUT);
-  pinMode(PINOUT_CCD_AO,INPUT);
+  pinMode(PINOUT_CCD_SI, OUTPUT);
+  pinMode(PINOUT_CCD_CLK, OUTPUT);
+  pinMode(PINOUT_CCD_AO, INPUT);
 
-  digitalWrite(PINOUT_CCD_SI,LOW);  // IDLE state
-  digitalWrite(PINOUT_CCD_CLK,LOW); // IDLE state
+  digitalWrite(PINOUT_CCD_SI, LOW);  // IDLE state
+  digitalWrite(PINOUT_CCD_CLK, LOW); // IDLE state
 }
 
 void captrueCCD(int explosureTimeMs) {
-  digitalWrite(PINOUT_CCD_CLK,LOW);
+  digitalWrite(PINOUT_CCD_CLK, LOW);
   delayMicroseconds(1);
-  digitalWrite(PINOUT_CCD_SI,HIGH);
-  delayMicroseconds(1);
-
-  digitalWrite(PINOUT_CCD_CLK,HIGH);
-  delayMicroseconds(1);
-  digitalWrite(PINOUT_CCD_SI,LOW);
+  digitalWrite(PINOUT_CCD_SI, HIGH);
   delayMicroseconds(1);
 
-  digitalWrite(PINOUT_CCD_CLK,LOW);
+  digitalWrite(PINOUT_CCD_CLK, HIGH);
+  delayMicroseconds(1);
+  digitalWrite(PINOUT_CCD_SI, LOW);
+  delayMicroseconds(1);
+
+  digitalWrite(PINOUT_CCD_CLK, LOW);
   delayMicroseconds(2);
 
   /* and now read the real image */
 
-  for (int i = 0; i<cNumPixels; i++) {
-    digitalWrite(PINOUT_CCD_CLK,HIGH);
+  for (int i = 0; i < cNumPixels; i++) {
+    digitalWrite(PINOUT_CCD_CLK, HIGH);
 
     delayMicroseconds(2);
     linearData[i] = analogRead(PINOUT_CCD_AO); // 8-bit is enough
-    digitalWrite(PINOUT_CCD_CLK,LOW);
+    digitalWrite(PINOUT_CCD_CLK, LOW);
     delayMicroseconds(2);
   }
 
-  digitalWrite(PINOUT_CCD_CLK,HIGH);
+  digitalWrite(PINOUT_CCD_CLK, HIGH);
   delayMicroseconds(2);
 
-  for (int t = 0; t<explosureTimeMs*250; t++) {
-    digitalWrite(PINOUT_CCD_CLK,LOW);
-    delayMicroseconds(2);
-
-    digitalWrite(PINOUT_CCD_CLK,HIGH);
-    delayMicroseconds(2);
-  }
+  delay(explosureTimeMs);
 }
 
 void printCCDLinearData(int maxVal) {
-  for (int i = 0; i<cNumPixels; i++) {
-    int t = floor(float(linearData[i])/float(maxVal)*10.0f-0.1f);
-    Serial.print(char(48+t));
+  for (int i = 0; i < cNumPixels; i++) {
+    int t = floor(float(linearData[i]) / float(maxVal) * 10.0f - 0.1f);
+    Serial.print(char(48 + t));
   }
   Serial.println();
 }
 
 void printCCDBinaryRawData() {
-  for (int i = 0; i<cNumPixels; i++) {
+  for (int i = 0; i < cNumPixels; i++) {
     char c = binaryData[i] ? 'x' : '-';
     Serial.print(c);
   }
@@ -105,26 +100,26 @@ void printCCDBinaryRawData() {
 }
 
 void printCCDOneHotData() {
-  for (int i = 0; i<cNumPixels; i++) {
+  for (int i = 0; i < cNumPixels; i++) {
     char c = binaryOnehotData[i] ? '^' : ' ';
     Serial.print(c);
   }
   Serial.println();
 }
 
-void processLinearVals(int& minVal,int& maxVal,int& avgVal,bool debug = false) {
+void parseLinearVals(int& minVal, int& maxVal, int& avgVal, bool debug = false) {
   maxVal = 0;
   minVal = 1e6;
 
-  for (int i = cCountStart; i<=cCountEnd; i++) {
+  for (int i = cCountStart; i <= cCountEnd; i++) {
     int currentVal = linearData[i];
 
-    if (maxVal<currentVal)
+    if (maxVal < currentVal)
       maxVal = currentVal;
-    if (minVal>currentVal)
+    if (minVal > currentVal)
       minVal = currentVal;
   }
-  avgVal = customRound(float(minVal+maxVal)/2.0f);
+  avgVal = customRound(float(minVal + maxVal) / 2.0f);
 
   if (debug) {
     Serial.print("avg: ");
@@ -138,21 +133,32 @@ void processLinearVals(int& minVal,int& maxVal,int& avgVal,bool debug = false) {
   }
 }
 
-void linearToBinary(int minVal,int maxVal,float usingThrehold) {
+void parseBinaryVals(int& blackNum, int& whiteNum, bool debug = false) {
+  blackNum = whiteNum = 0;
 
-  for (int i = 0; i<cNumPixels; i++) {
-    binaryData[i] = false;
+  for (int i = cCountStart; i <= cCountEnd; i++) {
+    bool currentVal = binaryData[i];
+
+    if (currentVal) {
+      blackNum++;
+    } else {
+      whiteNum++;
+    }
   }
+}
 
-  for (int i = cCountStart; i<=cCountEnd; i++) {
+void linearToBinary(int minVal, int maxVal, float usingThrehold) {
+  memset(binaryData, 0, sizeof(binaryData));
+
+  for (int i = cCountStart; i <= cCountEnd; i++) {
     binaryData[i] =
-      (float(linearData[i])<(minVal+(maxVal-minVal)*usingThrehold)) ? true : false;
+        (float(linearData[i]) < (minVal + (maxVal - minVal) * usingThrehold)) ? true : false;
   }
 }
 
 void drawOneHot(int point) {
-  for (int i = 0; i<cNumPixels; i++) {
-    if (i==point) {
+  for (int i = 0; i < cNumPixels; i++) {
+    if (i == point) {
       binaryOnehotData[i] = true;
       continue;
     }
@@ -163,16 +169,16 @@ void drawOneHot(int point) {
 int getTrackMidPixel() {
   int accumulatedDarkPixel = 0;
 
-  int trackLeftPixel = -1;
+  int trackLeftPixel  = -1;
   int trackRightPixel = -1;
-  int trackMidPixel = -1;
+  int trackMidPixel   = -1;
 
-  for (int i = cCountStart; i<=cCountEnd; i++) {
+  for (int i = cCountStart; i <= cCountEnd; i++) {
     bool currentPixel = binaryData[i];
 
     // Dark pixel
-    if (currentPixel==true) {
-      if (trackLeftPixel==-1) {
+    if (currentPixel == true) {
+      if (trackLeftPixel == -1) {
         trackLeftPixel = i;
       }
       accumulatedDarkPixel++;
@@ -180,35 +186,35 @@ int getTrackMidPixel() {
 
     // White pixel
     else {
-      if (accumulatedDarkPixel>=customRound(cEffectiveLineWidthMin)) {
-        trackRightPixel = i-1;
+      if (accumulatedDarkPixel >= customRound(cEffectiveLineWidthMin)) {
+        trackRightPixel = i - 1;
         break;
       }
 
       accumulatedDarkPixel = 0;
-      trackLeftPixel = -1;
+      trackLeftPixel       = -1;
     }
   }
 
   // Reaches the end
-  if (trackRightPixel==-1&&accumulatedDarkPixel>=customRound(cEffectiveLineWidthMin)) {
+  if (trackRightPixel == -1 && accumulatedDarkPixel >= customRound(cEffectiveLineWidthMin)) {
     trackRightPixel = cCountEnd;
   }
 
   // Nothing valid
-  if (trackLeftPixel==-1||trackRightPixel==-1)
+  if (trackLeftPixel == -1 || trackRightPixel == -1)
     return -1;
 
   // Get mid point
-  if ((trackRightPixel-trackLeftPixel)%2==0) {
-    trackMidPixel = customRound((trackRightPixel+trackLeftPixel)/2.0f);
+  if ((trackRightPixel - trackLeftPixel) % 2 == 0) {
+    trackMidPixel = customRound((trackRightPixel + trackLeftPixel) / 2.0f);
   } else {
-    int trackMidPixelCandidate1 = customRound((trackRightPixel+trackLeftPixel-1)/2.0f);
-    int trackMidPixelCandidate2 = trackMidPixelCandidate1+1;
+    int trackMidPixelCandidate1 = customRound((trackRightPixel + trackLeftPixel - 1) / 2.0f);
+    int trackMidPixelCandidate2 = trackMidPixelCandidate1 + 1;
 
-    trackMidPixel = (linearData[trackMidPixelCandidate1]<linearData[trackMidPixelCandidate2])
-      ? trackMidPixelCandidate1
-      : trackMidPixelCandidate2;
+    trackMidPixel = (linearData[trackMidPixelCandidate1] < linearData[trackMidPixelCandidate2])
+                        ? trackMidPixelCandidate1
+                        : trackMidPixelCandidate2;
   }
 
   return trackMidPixel;
@@ -223,34 +229,32 @@ struct explosureRecord {
   float contrast;
 };
 
-int bestAvg = 0;
-
-void getBestExplosureTime(explosureRecord& bestRecord,bool& cameraIsBlocked,bool& bestAvailable,
-  bool debug = false) {
+void getBestExplosureTime(explosureRecord& bestRecord, bool& cameraIsBlocked, bool& bestAvailable,
+                          bool debug = false) {
   cameraIsBlocked = false;
-  bestAvailable = false;
+  bestAvailable   = false;
 
   // Clear previous val
   captrueCCD(cDefaultExplosureTime);
 
-  int recordSize = (cExplosureTimeEnd-cExplosureTimeStart)/cExplosureTimePropagation+1;
+  int recordSize = (cExplosureTimeEnd - cExplosureTimeStart) / cExplosureTimePropagation + 1;
   float threholdsForEachExplosureTime[recordSize];
   float minMaxRatioResults[recordSize];
   explosureRecord records[recordSize];
 
   // Test for each explosuring time
-  for (uint8_t i = 0; i<recordSize; i++) {
+  for (uint8_t i = 0; i < recordSize; i++) {
     explosureRecord& thisRecord = records[i];
 
     int& thisExplosureTime = thisRecord.explosureTime;
-    int& thisMinVal = thisRecord.minVal;
-    int& thisMaxVal = thisRecord.maxVal;
-    int& thisAvgVal = thisRecord.avgVal;
-    float& thisThrehold = thisRecord.threhold;
-    float& thisContrast = thisRecord.contrast;
+    int& thisMinVal        = thisRecord.minVal;
+    int& thisMaxVal        = thisRecord.maxVal;
+    int& thisAvgVal        = thisRecord.avgVal;
+    float& thisThrehold    = thisRecord.threhold;
+    float& thisContrast    = thisRecord.contrast;
 
-    thisThrehold = -1;
-    thisExplosureTime = cExplosureTimeStart+cExplosureTimePropagation*i;
+    thisThrehold      = -1;
+    thisExplosureTime = cExplosureTimeStart + cExplosureTimePropagation * i;
 
     if (debug) {
       Serial.print("explosure time: ");
@@ -261,16 +265,16 @@ void getBestExplosureTime(explosureRecord& bestRecord,bool& cameraIsBlocked,bool
     captrueCCD(thisExplosureTime);
     captrueCCD(thisExplosureTime);
 
-    processLinearVals(thisMinVal,thisMaxVal,thisAvgVal,debug);
-    thisContrast = (thisMaxVal==0) ? 0 : float(thisMinVal)/float(thisMaxVal);
+    parseLinearVals(thisMinVal, thisMaxVal, thisAvgVal, debug);
+    thisContrast = (thisMaxVal == 0) ? 0 : float(thisMinVal) / float(thisMaxVal);
 
     // Find track dynamically
-    for (float testThrehold = 0.0f; testThrehold<1.0f;
-      testThrehold += cThreholdSearchingPropagationInit) {
-      linearToBinary(thisMinVal,thisMaxVal,testThrehold);
+    for (float testThrehold = 0.0f; testThrehold < 1.0f;
+         testThrehold += cThreholdSearchingPropagationInit) {
+      linearToBinary(thisMinVal, thisMaxVal, testThrehold);
       int trackMidPixel = getTrackMidPixel();
 
-      if (trackMidPixel!=-1) {
+      if (trackMidPixel != -1) {
         thisThrehold = testThrehold;
 
         if (debug) {
@@ -282,26 +286,26 @@ void getBestExplosureTime(explosureRecord& bestRecord,bool& cameraIsBlocked,bool
         break;
       }
     }
-    if (thisThrehold==-1&&debug)
+    if (thisThrehold == -1 && debug)
       Serial.println("Failed to find threhold for this explosure time!");
   }
 
   // Find explosure time with minimum ratio
-  float minThrehold = 1.0f;
+  float threhold    = 1.0f;
   float minContrast = 1.0f;
   float maxContrast = 0.0f;
 
-  for (uint8_t i = 0; i<recordSize; i++) {
+  for (uint8_t i = 0; i < recordSize; i++) {
     explosureRecord& thisRecord = records[i];
 
     int& thisExplosureTime = thisRecord.explosureTime;
-    int& thisMinVal = thisRecord.minVal;
-    int& thisMaxVal = thisRecord.maxVal;
-    int& thisAvgVal = thisRecord.avgVal;
-    float& thisThrehold = thisRecord.threhold;
-    float& thisContrast = thisRecord.contrast;
+    int& thisMinVal        = thisRecord.minVal;
+    int& thisMaxVal        = thisRecord.maxVal;
+    int& thisAvgVal        = thisRecord.avgVal;
+    float& thisThrehold    = thisRecord.threhold;
+    float& thisContrast    = thisRecord.contrast;
 
-    if (thisThrehold==-1)
+    if (thisThrehold == -1)
       continue;
 
     Serial.print("explosure_time: ");
@@ -311,84 +315,63 @@ void getBestExplosureTime(explosureRecord& bestRecord,bool& cameraIsBlocked,bool
     Serial.print("  contrast: ");
     Serial.println(thisContrast);
 
-    if (thisThrehold<minThrehold) {
-      minThrehold = thisThrehold;
+    if (thisThrehold < threhold) {
+      threhold = thisThrehold;
       {
         bestRecord.explosureTime = thisExplosureTime;
-        bestRecord.minVal = thisMinVal;
-        bestRecord.maxVal = thisMaxVal;
-        bestRecord.avgVal = thisAvgVal;
-        bestRecord.threhold = thisThrehold;
-        bestRecord.contrast = thisContrast;
+        bestRecord.minVal        = thisMinVal;
+        bestRecord.maxVal        = thisMaxVal;
+        bestRecord.avgVal        = thisAvgVal;
+        bestRecord.threhold      = thisThrehold;
+        bestRecord.contrast      = thisContrast;
       }
       bestAvailable = true;
     }
 
-    if (thisContrast<minContrast)
+    if (thisContrast < minContrast)
       minContrast = thisContrast;
 
-    if (thisContrast>maxContrast)
+    if (thisContrast > maxContrast)
       maxContrast = thisContrast;
   }
 
-  bestAvg = bestRecord.avgVal;
-  Serial.print("bestAvg ");
-  Serial.println(bestAvg);
-
   // Output
-  cameraIsBlocked = !bestAvailable||
-    (minContrast < 0.02&&maxContrast-minContrast > cMinMaxRatioDeltaBlocked);
+  cameraIsBlocked = !bestAvailable ||
+                    (minContrast < 0.02 && maxContrast - minContrast > cMinMaxRatioDeltaBlocked);
 }
 
-void processCCD(int& trackMidPixel,float& usingThrehold,int& tracingStatus,int explosureTime,
-  float minThrehold,bool debug = false) {
-
-  int minVal = 0;
-  int maxVal = 0;
-  int avgVal = 0;
+void processCCD(int& trackMidPixel, int& tracingStatus, int explosureTime, float threhold,
+                bool debug = false) {
 
   tracingStatus = STATUS_NORMAL;
 
   // Capture
   captrueCCD(explosureTime);
-  captrueCCD(explosureTime);
 
-  // Parse linear data
-  processLinearVals(minVal,maxVal,avgVal,debug);
+  int minVal, maxVal, avgVal;
+  parseLinearVals(minVal, maxVal, avgVal, debug);
 
-  float solidRatio = (float)avgVal/(float)bestAvg;
-  oledPrint(solidRatio,"solidRto",2);
-  if (solidRatio<cBlockingConditionRatio) {
+  linearToBinary(minVal, maxVal, threhold);
+
+  int blackNum, whiteNum;
+  parseBinaryVals(blackNum, whiteNum);
+
+  if (blackNum > whiteNum) {
     boardLedOn();
     tracingStatus = STATUS_PLATFORM;
+    return;
   } else {
     boardLedOff();
   }
 
-  // Find track dynamically, starting from the best ratio
-  for (float testThrehold = minThrehold; testThrehold<1.0f;
-    testThrehold += cThreholdSearchingPropagation) {
-    linearToBinary(minVal,maxVal,testThrehold);
-    trackMidPixel = getTrackMidPixel();
+  trackMidPixel = getTrackMidPixel();
 
-    if (trackMidPixel!=-1) {
-      // Compare ratio
-      if (testThrehold>min(max(minThrehold*10,cDarkRatioAbnormalMin),cDarkRatioAbnormalMax)) {
-        tracingStatus = STATUS_HIGH_DL;
-      }
-
-      if (debug)
-        drawOneHot(trackMidPixel);
-
-      usingThrehold = testThrehold;
-      break;
-    }
-  }
+  if (trackMidPixel != -1 && debug)
+    drawOneHot(trackMidPixel);
 
   // Cannot find track
-  if (trackMidPixel==-1) {
+  if (trackMidPixel == -1) {
     tracingStatus = STATUS_NO_TRACK;
-    usingThrehold = 0;
     return;
   }
 
@@ -398,7 +381,7 @@ void processCCD(int& trackMidPixel,float& usingThrehold,int& tracingStatus,int e
     printCCDOneHotData();
   }
 
-  // Map pixel
+  // Pixel mapping
   trackMidPixel =
-    customRound(map(float(trackMidPixel),float(cCountStart),float(cCountEnd),0.0f,128.0f));
+      customRound(map(float(trackMidPixel), float(cCountStart), float(cCountEnd), 0.0f, 128.0f));
 }
